6ef1463441012b9306df5347ae30ab1bd655c578
MStasiw <crazy-mikey@hotmail.com>
2014-02-17 13:43:54 -0500

    added Administrator.java class

---
+++ src/slap/Administrator.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package slap;
+
+/**
+ * @author Michael
+ *
+ */
+class Administrator extends Account {
+
+	/**
+	 * @param firstName
+	 * @param lastName
+	 * @param username
+	 * @param password
+	 */
+	public Administrator(String firstName, String lastName, String username,
+			String password) {
+		super(firstName, lastName, username, password, Role.administrator);
+		// TODO Auto-generated constructor stub
+	}
+
+}
