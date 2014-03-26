002a7b59eb94078b0efdcbc3ac64d86ffe125e68
Alden Ozburn <aldendino@gmail.com>
2014-03-26 19:45:20 -0400

    Menu Structure added

--- src/slap/SLAPFrame.java
+++ src/slap/SLAPFrame.java
@@ -10,9 +10,16 @@ public class SLAPFrame extends JFrame {
     private final int FRAME_HEIGHT = 600 ;
     private final int MIN_FRAME_WIDTH = 500 ;
     private final int MIN_FRAME_HEIGHT = 300 ;
+    
+    private JMenuBar menuBar ;
+    private JMenu accountMenu ;
+    private JMenu coursesMenu ;
+    private JMenuItem signinMenuItem ;
+    private JMenuItem signoutMenuItem ;
 	
 	public SLAPFrame() {
 		initialize() ;
+		setupMenuBar() ;
 		
 		setVisible(true) ;
 	}	
@@ -23,5 +30,34 @@ public class SLAPFrame extends JFrame {
         setMinimumSize(new Dimension(MIN_FRAME_WIDTH, MIN_FRAME_HEIGHT)) ;
         setResizable(true) ;
         setDefaultCloseOperation(EXIT_ON_CLOSE) ;        
+	}
+	
+	private void setupMenuBar() {
+		menuBar = new JMenuBar() ;
+		setupAccountMenu(menuBar) ;
+		setupCoursesMenu(menuBar) ;
+		setJMenuBar(menuBar) ;
+	}
+	
+	private void setupAccountMenu(JMenuBar jmb) {
+		accountMenu = new JMenu("Account") ;
+		setupSigninMenuItem(accountMenu) ;
+		setupSignoutMenuItem(accountMenu) ;
+		jmb.add(accountMenu) ;
+	}
+	
+	private void setupCoursesMenu(JMenuBar jmb) {
+		coursesMenu = new JMenu("Courses") ;
+		jmb.add(coursesMenu) ;
+	}
+	
+	private void setupSigninMenuItem(JMenu jm) {
+		signinMenuItem = new JMenuItem("Sign in") ;
+		jm.add(signinMenuItem) ;
+	}
+	
+	private void setupSignoutMenuItem(JMenu jm) {
+		signoutMenuItem = new JMenuItem("Sign out") ;
+		jm.add(signoutMenuItem) ;
 	}
 }
