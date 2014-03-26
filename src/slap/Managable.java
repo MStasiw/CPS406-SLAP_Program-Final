4edeab2cc0df86f1c9ddb593f5b1fd10b09fec46
Alden Ozburn <aldendino@gmail.com>
2014-03-26 18:55:30 -0400

    Added Manager class with its corresponding Managable interface
    Use each Manager with a Class that implements Managable and the type of
    Comparable that said Class returns from its implementation of the
    Mangable method “getID”.

---
+++ src/slap/Managable.java
@@ -0,0 +1,10 @@
+package slap;
+
+/**
+ *  Interface to define a manageable item which can be used by a Manager of the same type
+ */
+public interface Managable
+{
+    @SuppressWarnings("rawtypes")
+	public Comparable getID() ;
+}
---
+++ src/slap/Manager.java
@@ -0,0 +1,71 @@
+package slap;
+import java.util.* ;
+import java.io.* ;
+
+/**
+ * A manager for any class that implements Manageable
+ */
+@SuppressWarnings("rawtypes")
+public class Manager<C extends Comparable, M extends Managable> implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	private TreeMap<C, M> items ;
+
+    public Manager()
+    {
+        items = new TreeMap<C, M>() ;
+    }
+
+    public Managable get(C id)
+    {
+        return items.get(id) ;
+    }
+
+    public boolean add(C c, M m)
+    {   
+        if(items.containsKey(c))
+        {
+            return false ;
+        }
+        items.put(c, m) ;        
+        return true ;
+    } 
+
+    public boolean remove(C id)
+    {
+        if(!items.containsKey(id))
+        {
+            return false ;
+        }
+        items.remove(id) ;
+        return true ;
+    }
+
+    public boolean contains(C id)
+    {
+        return items.containsKey(id) ;
+    }
+
+    public int size()
+    {
+        return items.size() ;
+    }
+
+    public ArrayList<Comparable> getIDArray()
+    {
+		ArrayList<Comparable> idArray = new ArrayList<Comparable>() ;
+        idArray.addAll(items.keySet()) ;
+        return idArray ;
+    }
+
+    public Managable[] getItemArray()
+    {
+        Managable[] itemArray = new Managable[size()] ;
+        int count = 0 ;
+        for(Map.Entry<C, M> entry : items.entrySet()) 
+        {
+            itemArray[count++] = (Managable) entry.getValue() ;
+        }
+        return itemArray ;
+    }
+}
