f224154018b5a550a048b5359a6a4304759a71cb
Alden Ozburn <aldendino@gmail.com>
2014-03-28 10:53:03 -0400

    Scrollable tabs are fixed and horizontally wrap

--- src/slap/SLAPTab.java
+++ src/slap/SLAPTab.java
@@ -1,7 +1,8 @@
 package slap;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 
 import javax.swing.* ;
@@ -10,18 +11,23 @@ import javax.swing.* ;
 public class SLAPTab extends JPanel {
 	
 	private JScrollPane scrollPane ;
-	private JPanel panel ;
+	private ScrollPanel panel ;
 	
 	private ArrayList<SLAPTabItem> items ;
 	
 	public SLAPTab() {
 		items = new ArrayList<SLAPTabItem>() ;
 		setLayout(new BorderLayout()) ;
-		panel = new JPanel() ;
+		panel = new ScrollPanel() ;
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS)) ;
-		scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER) ;
-		add(scrollPane, BorderLayout.CENTER) ;
-		//add(panel) ;
+		/*scrollPane = new JScrollPane(panel, 
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER) ;
+		add(scrollPane, BorderLayout.CENTER) ;*/
+		scrollPane = new JScrollPane() ;
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER) ;
+		scrollPane.setViewportView(panel);
+		add(scrollPane) ;
 	}
 	
 	public void addItem(SLAPTabItem item) {
@@ -33,6 +39,40 @@ public class SLAPTab extends JPanel {
 		panel.removeAll() ;
 		for(SLAPTabItem item : items) {
 			panel.add(item) ;
+		}
+	}
+
+	class ScrollPanel extends JPanel implements Scrollable {
+		
+		public ScrollPanel() {
+			super() ;
+		}	
+		
+		@Override
+		public Dimension getPreferredScrollableViewportSize() {
+			return getPreferredSize() ;
+		}
+
+		@Override
+		public int getScrollableUnitIncrement(Rectangle visibleRect,
+				int orientation, int direction) {
+			return 10 ;
+		}
+
+		@Override
+		public int getScrollableBlockIncrement(Rectangle visibleRect,
+				int orientation, int direction) {
+			return 20 ;
+		}
+
+		@Override
+		public boolean getScrollableTracksViewportWidth() {
+			return true ;
+		}
+
+		@Override
+		public boolean getScrollableTracksViewportHeight() {
+			return false ;
 		}
 	}
 }
