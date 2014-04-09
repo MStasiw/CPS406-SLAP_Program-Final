00d6d0c8943e271a401680c586cea2fee00eaab5
kevinbraunstein <kevin.braunstein@ryerson.ca>
2014-04-09 18:01:37 -0400

    Assignment tab again...

---
+++ src/slap/SLAPAssignInstructionsStudentTab.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package slap;
+
+import javax.swing.*;
+
+/**
+ * @author Kevin
+ *
+ */
+@SuppressWarnings("serial")
+public class SLAPAssignInstructionsStudentTab extends JPanel{
+
+	public SLAPAssignInstructionsStudentTab(SLAP slap, SLAPFrame frame) {
+		
+	}
+}
--- src/slap/SLAPAssignmentTab.java
+++ src/slap/SLAPAssignmentTab.java
@@ -19,26 +19,31 @@ public class SLAPAssignmentTab extends JPanel{
 	private SLAP slap ;
 	private SLAPFrame frame;
 	private JPanel mainPanel;
+	private JPanel selectAssign;
+	private JTabbedPane selectDisplay;
 	
-	private SLAPAssignInstructionsStudent studentInstruct;
+	private SLAPAssignInstructionsStudentTab studentInstruct;
 	
 	public SLAPAssignmentTab(SLAPFrame sframe, SLAP s) {
 		this.slap = s;
 		this.frame = sframe;
 		
 		mainPanel = new JPanel();
-		studentInstruct = new SLAPAssignInstructionsStudent(slap, frame);
+		studentInstruct = new SLAPAssignInstructionsStudentTab(slap, frame);
 		
 		setLayout(new BorderLayout());
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		
+		setUpMainAssignmentGUI();
 	}
 	
-	private void setupStudentAssignmentGUI() {
+	private void setUpMainAssignmentGUI() {
 		//Set up the JComboBox used to select a course
-		JPanel selectAssign = new JPanel();
+		selectAssign = new JPanel();
 		selectAssign.setLayout(new BoxLayout(selectAssign, BoxLayout.X_AXIS));
 		JLabel comboLabel = new JLabel();
-		comboLabel.setText("Select an assignment: ");
+		comboLabel.setText("Select an Assignment: ");
+		@SuppressWarnings("rawtypes")
 		JComboBox selectCombo = new JComboBox();
 		
 		selectCombo.setPreferredSize(new Dimension(300, 25));
@@ -49,10 +54,13 @@ public class SLAPAssignmentTab extends JPanel{
 		selectAssign.add(Box.createHorizontalGlue()) ;
 		
 		//Set up the JTabbedPane to the left
-		JTabbedPane selectDisplay = new JTabbedPane(JTabbedPane.LEFT, JTabbedPane.SCROLL_TAB_LAYOUT);
+		selectDisplay = new JTabbedPane(JTabbedPane.LEFT, JTabbedPane.SCROLL_TAB_LAYOUT);
+	}
+	
+	private void setupStudentAssignmentGUI() {
 		SLAPAdminUserTab userTab = new SLAPAdminUserTab(slap, frame) ;
-		selectDisplay.addTab("Instructions", userTab);
-		selectDisplay.addTab("Submit", studentInstruct);
+		selectDisplay.addTab("Instructions", studentInstruct);
+		selectDisplay.addTab("Submit", userTab);
 		mainPanel.add(Box.createVerticalStrut(10));
 		mainPanel.add(selectAssign);
 		mainPanel.add(Box.createVerticalStrut(10));
@@ -72,7 +80,9 @@ public class SLAPAssignmentTab extends JPanel{
 	 * Refresh the items in the tab
 	 */
 	protected void refresh() {
-		removeAll() ;
+		removeAll();
+		mainPanel.removeAll();
+		selectDisplay.removeAll();
 		
 		if(slap.getCurrentUser() != null) { //check if user is null, if so, there isn't anyone logged in
 			if(slap.getCurrentUser().getRole() == Role.student) {
@@ -85,6 +95,6 @@ public class SLAPAssignmentTab extends JPanel{
 		}
 		
 		this.validate();
-		frame.refresh() ;
+		frame.refresh();
 	}
 }
